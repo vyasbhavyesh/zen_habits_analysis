7c2a0fcc53eb1f89671292dd103ac8537e19e814
Bhavyesh Vyas <bhavyas@publicisgroupe.net>
2018-08-27 12:06:21 +0530

    nlp analysis file

---
+++ nlp_analysis.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 27 11:56:32 2018
+
+@author: bhavyas
+"""
+ import nltk
+ import gensim
+ import numpy as np
+ import pandas as pd
+ 
+ #reading scraped blog from csv to dataframe 
+ blog_df = pd.read_csv('')
+ 
